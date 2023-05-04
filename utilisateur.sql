3504b4e0a5faa8c534221369099da27f6c2427da
Meuret Florian <florian.meuret0@gmail.com>
2023-05-04 11:33:29 +0200

    ajout fichier sql

---
+++ utilisateur.sql
@@ -0,0 +1,191 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 04 mai 2023 à 09:30
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `utilisateur`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `emploi`
+--
+
+DROP TABLE IF EXISTS `emploi`;
+CREATE TABLE IF NOT EXISTS `emploi` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `emploi`
+--
+
+INSERT INTO `emploi` (`id`, `name`) VALUES
+(1, 'Developpeur'),
+(2, 'Testeur'),
+(3, 'Chef de projet');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entreprise`
+--
+
+DROP TABLE IF EXISTS `entreprise`;
+CREATE TABLE IF NOT EXISTS `entreprise` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `entreprise`
+--
+
+INSERT INTO `entreprise` (`id`, `nom`) VALUES
+(1, 'Amazon'),
+(2, 'Google'),
+(3, 'Red Hat');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pays`
+--
+
+DROP TABLE IF EXISTS `pays`;
+CREATE TABLE IF NOT EXISTS `pays` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `pays`
+--
+
+INSERT INTO `pays` (`id`, `name`) VALUES
+(1, 'France'),
+(2, 'Allemagne'),
+(3, 'Espagne');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recherche_emploi_utilisateur`
+--
+
+DROP TABLE IF EXISTS `recherche_emploi_utilisateur`;
+CREATE TABLE IF NOT EXISTS `recherche_emploi_utilisateur` (
+  `user_id` int NOT NULL,
+  `emploi_id` int NOT NULL,
+  PRIMARY KEY (`user_id`,`emploi_id`),
+  KEY `FKm97nfoc6ask1llg09q3mppqre` (`emploi_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `recherche_emploi_utilisateur`
+--
+
+INSERT INTO `recherche_emploi_utilisateur` (`user_id`, `emploi_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`id`, `name`) VALUES
+(1, 'ROLE_UTILISATEUR'),
+(2, 'ROLE_ADMINISTRATEUR');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `age` int NOT NULL,
+  `created_at` date DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `first_name` varchar(80) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `nom_image_profil` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `updated_at` date DEFAULT NULL,
+  `entreprise_id` int DEFAULT NULL,
+  `pays_id` int DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK2byqabrd53pa1wtecx49bao2f` (`entreprise_id`),
+  KEY `FKikn98wim6gs015nd0c3h90j37` (`pays_id`),
+  KEY `FKn82ha3ccdebhokx3a8fgdqeyy` (`role_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `age`, `created_at`, `email`, `first_name`, `name`, `nom_image_profil`, `password`, `updated_at`, `entreprise_id`, `pays_id`, `role_id`) VALUES
+(1, 5, '2023-05-03', 'a@a.com', 'Toto', 'TUTU', NULL, '$2a$10$wXW2wHA2bu1TdQ26p.2UoehWv8m92w88kabSeL.348VqkpWvSt51q', '2023-05-03', 1, 1, 2),
+(2, 10, '2023-05-03', 'b@b.com', 'Titi', 'SMITH', NULL, '$2a$10$wXW2wHA2bu1TdQ26p.2UoehWv8m92w88kabSeL.348VqkpWvSt51q', '2023-05-03', 2, 2, 1),
+(3, 15, '2023-05-03', 'c@c.com', 'Tata', 'SMITH', NULL, '$2a$10$wXW2wHA2bu1TdQ26p.2UoehWv8m92w88kabSeL.348VqkpWvSt51q', '2023-05-03', 3, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `login` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`login`, `password`) VALUES
+('Login', 'Password');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
